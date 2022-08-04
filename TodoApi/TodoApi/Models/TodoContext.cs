24741878bafec336f233ee41d04b39f171a8c887
CVHOANG-coder <mrgreencat99@gmail.com>
2022-08-04 17:37:20 +0700

    'firstcommit'

--- TodoApi/TodoApi/Models/TodoContext.cs
+++ TodoApi/TodoApi/Models/TodoContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TodoApi.Models
 {
-    public class Class
+    public class TodoContext : DbContext
     {
+        public TodoContext(DbContextOptions<TodoContext> options):base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; } = null;
     }
 }
