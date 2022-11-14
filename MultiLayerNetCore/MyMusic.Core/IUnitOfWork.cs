598864ba950445c653f86e13f7c0928516829eeb
CVHOANG-coder <mrgreencat99@gmail.com>
2022-11-14 17:20:31 +0700

    'n'

---
+++ MultiLayerNetCore/MyMusic.Core/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMusic.Core.Repositories;
+
+namespace MyMusic.Core
+
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IMusicRepository Music { get; }
+        IArtistRepository Artist { get; }
+        Task<int> CommitAsync();
+    }
+}
---
+++ MultiLayerNetCore/MyMusic.Data/Configurations/MusicConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyMusic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Data.Configurations
+{
+    public class MusicConfiguration:IEntityTypeConfiguration<Music>
+    {
+        public void Configure(EntityTypeBuilder<Music> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .UseIdentityColumn();
+            builder.Property(x=>x.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.HasOne(x=>x.Artist)
+                .WithMany(a=>a.Musics)
+                .HasForeignKey(x=>x.ArtistId);
+            builder.ToTable("Musics");
+        }
+    }
+}
